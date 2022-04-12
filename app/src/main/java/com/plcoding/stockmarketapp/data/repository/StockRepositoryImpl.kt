81390e45b9a5781c26f2e31e108bb6ebc8b9100f
Avneet <avneetmaankiya@gmail.com>
2022-04-13 02:42:25 +0530

    Stock cache

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/repository/StockRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.plcoding.stockmarketapp.data.repository
+
+import com.plcoding.stockmarketapp.data.local.StockDatabase
+import com.plcoding.stockmarketapp.data.mappers.toCompanyListing
+import com.plcoding.stockmarketapp.data.remote.StockApi
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.repository.StockRepository
+import com.plcoding.stockmarketapp.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class StockRepositoryImpl @Inject constructor(
+    val api: StockApi,
+    val db: StockDatabase
+): StockRepository{
+    private val dao = db.dao
+
+    override suspend fun getcompanyListings(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>> {
+        return flow{
+            emit(Resource.Loading(true))
+            val localListing = dao.searchCompanyListing(query)
+            emit(Resource.Success(
+                data = localListing.map{ it.toCompanyListing() }
+            ))
+
+            val isDbEmpty = localListing.isEmpty() && query.isBlank()
+            val shouldJustLoadFromCache = !isDbEmpty && !fetchFromRemote
+
+            if(shouldJustLoadFromCache){
+                emit(Resource.Loading(false))
+                return@flow
+            }
+
+            val remoteListings = try {
+                val response = api.getListings()
+                response.byteStream()
+            } catch (e: IOException){
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            } catch (e: HttpException){
+                e.printStackTrace()
+                emit(Resource.Error("Couldn't load data"))
+            }
+        }
+    }
+
+}
